ff4438d0dc64c75fea2541ff35f00ba1b8f975c6
vermashubham45 <109742551+vermashubham45@users.noreply.github.com>
2023-04-24 01:54:33 +0530

    Add files via upload

---
+++ CHT CS TL PR Assignnment.js
@@ -0,0 +1,208 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record','N/search','N/ui/dialog','N/log'],
+
+function(record,search,dialog,log) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+	    	alert('IN Save record ');
+    		var currentRecord = scriptContext.currentRecord;
+        	var fieldValue = currentRecord.getValue('custentity_claim_number_sv');
+    		if (fieldValue) {
+       		 var entitySearchObj = search.create({
+       			   type: "entity",
+       			   filters:
+       			   [
+       			      ["custentity_claim_number_sv","is",fieldValue]
+       			   ],
+       			   columns:
+       			   [
+       			      search.createColumn({name: "custentity_claim_number_sv", label: "Claim Number SV"})
+       			   ]
+       			});
+       		    var claim_num;
+       			var searchResultCount = entitySearchObj.runPaged().count;
+       			log.debug("entitySearchObj result count",searchResultCount);
+       			entitySearchObj.run().each(function(result){
+       			   // .run().each has a limit of 4,000 results
+       				claim_num=result.getValue('custentity_claim_number_sv');
+       			   return true;
+       			});
+       		
+       		alert("jobSearchObj result count"+searchResultCount);
+       	    if (fieldValue === claim_num) {
+       	    	dialog.alert({
+       	    		title:'Error',
+       	    		message:'Claim Number is alredy exist in your Record'
+       	    	});
+       	      //alert('Claim Number is alredy exist in your Record');
+       	      return false;
+       	      
+       	    } 
+       	    else{
+         		  return true;
+       	     }
+       	   }
+	    
+    	 
+
+    }
+    return {
+        pageInit: pageInit,
+//        fieldChanged: fieldChanged,
+//        postSourcing: postSourcing,
+//        sublistChanged: sublistChanged,
+//        lineInit: lineInit,
+//        validateField: validateField,
+//        validateLine: validateLine,
+//        validateInsert: validateInsert,
+//        validateDelete: validateDelete,
+        saveRecord: saveRecord
+    };
+    
+});
